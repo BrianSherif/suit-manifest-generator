e43d3f7588ca237ac248afd077bb8d9dd23e2bc8
briansherif <briansherif@yahoo.com>
2020-05-02 09:01:13 -0400

    Created a minimal parser to be used for testing purposes.
    Signed-off-by: briansherif <briansherif@yahoo.com>

---
+++ bin/minimal-parse-manifest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright 2019 ARM Limited or its affiliates
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+import os
+import sys
+
+from suit_tool.parse import main as parser
+
+suittoolPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+sys.path.insert(0, suittoolPath)
+
+
+class Emptyopts:
+    def __init__(self, manifest_name):
+        self.json = True
+        self.manifest = open(manifest_name, 'rb')
+
+
+if __name__ == "__main__":
+    manifest_name = '../examples/testcases/testcase0.cbor'
+    sys.exit(parser(Emptyopts(manifest_name)))
